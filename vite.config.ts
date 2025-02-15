8c19cda1122604657f71066fede90fc112f69497
Trix90 <riskysells@gmail.com>
2025-02-15 22:43:25 +0300

    Remove Vercel configuration and update Vite config for React integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.js
-export default {
-    base: '/',
-    build: {
-      outDir: 'dist', // This tells Vite to output to the 'dist' folder
-    },
-  };
-  
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
