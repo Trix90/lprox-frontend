e32a4ae828a97647defcc4ce597f5f49cb08a65d
Trix90 <riskysells@gmail.com>
2025-02-15 22:41:48 +0300

    starting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+    base: '/',
+    build: {
+      outDir: 'dist', // This tells Vite to output to the 'dist' folder
+    },
+  };
+  
